81f0842cfee213172ff42515d93c3d4e6a73b2e2
npentico <npentico16@gmail.com>
2023-09-07 20:05:25 -0700

    preliminary confirm button for tower purchasing
    -Button to confirm tower purchase works still very wip
    -Need to get image from prefab and put it into button
    -What to do when not enough money etc.

--- Tower_defense_game/Assets/Scripts/Construction.cs
+++ Tower_defense_game/Assets/Scripts/Construction.cs
@@ -1,24 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+
 
 public class Construction : MonoBehaviour
 {
    [SerializeField] List<GameObject> towersCanSpawnPrefabs;
    [SerializeField] float yOffset = 1.15f;
+
+   [SerializeField] GameObject spriteCanvas;
+
+   [SerializeField] Image spriteImage;
+
+   [SerializeField]GameObject checkMark;
+
+   bool showingButtons = false;
+   Camera m_Camera;
    
 
     
 
     public void ConstructorButtonClicked(){
-        Debug.Log("ClickedConstructor");
+        
+        spriteCanvas.SetActive(!spriteCanvas.activeSelf);
     }
+
+   void Awake()
+   {
+       m_Camera = Camera.main;
+   }
+
+   //WIP TO TURN OFF BUTTONS WHEN YOU CLICK SOMETHING NOT THE TOWER OR THE UI ELEMENTS
+   /*void Update()
+   {
+       if (Input.GetMouseButtonDown(0))
+       {
+        
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = Camera.main.nearClipPlane;
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+           RaycastHit2D hit2d =  Physics2D.Raycast(m_Camera.transform.position, worldPosition);
+        
+           
+            if(hit2d.collider != null && hit2d.collider.gameObject == gameObject){
+                Debug.Log(hit2d.transform.gameObject.name);
+                spriteCanvas.SetActive(true);
+            }
+            
+            //if you click nothing
+            else {
+                spriteCanvas.SetActive(false);  
+            }
+           
+             
+       }
+   }*/
 
     void OnMouseDown()
    {
        ConstructorButtonClicked();
-       UiManager.instance.ConstructorTowerWasClicked(gameObject);
-       BuyTower(towersCanSpawnPrefabs[0]);
+      
+
+    //   UiManager.instance.ConstructorTowerWasClicked(gameObject);
+      // BuyTower(towersCanSpawnPrefabs[0]);
    }
 
     //the hard commit will create the tower
@@ -37,4 +85,19 @@ public class Construction : MonoBehaviour
    public List<GameObject> getTowers(){
     return towersCanSpawnPrefabs;
    }
+
+   public void ClickedLittleButton(){
+    EnableCheckMark();
+
+   }
+
+   public void ClickedCheckMark(){
+    Debug.Log("Clicked Check Mark");
+    BuyTower(towersCanSpawnPrefabs[0]);
+   }
+
+   public void DisableCheckMark(){ checkMark.SetActive(false); }
+   public void EnableCheckMark() { checkMark.SetActive(true); }
+
+   
 }
